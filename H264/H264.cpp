fc1f94bfea88126b6e9ef7da259160cf476c7bef
YSS <ms0697310@outlook.com>
2024-12-14 19:55:47 +0800

    init

---
+++ H264/H264.cpp
@@ -0,0 +1,64 @@
+﻿#include <SDL.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    // 初始化 SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    // 創建窗口
+    SDL_Window* window = SDL_CreateWindow(
+        "SDL Tutorial",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        800, 600,
+        SDL_WINDOW_SHOWN
+    );
+
+    if (!window) {
+        std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return -1;
+    }
+
+    // 創建渲染器
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (!renderer) {
+        std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    // 主循環
+    bool isRunning = true;
+    SDL_Event event;
+    while (isRunning) {
+        // 處理事件
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                isRunning = false;
+            }
+        }
+
+        // 清屏
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        // 繪製矩形
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_Rect rect = { 300, 200, 200, 150 };
+        SDL_RenderFillRect(renderer, &rect);
+
+        // 顯示渲染結果
+        SDL_RenderPresent(renderer);
+    }
+
+    // 清理資源
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
